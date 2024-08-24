2a0440b4dca8cde5fb34c69eb682c7dbdcb452fd
Zain <zainhmoustafa@gmail.com>
2024-08-23 22:05:50 -0400

    invoke vercel

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 const path = require('path');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Add an alias for the public directory
-    config.resolve.alias['public'] = path.join(__dirname, 'public');
-
-    return config;
-  },
-};
-
-const path = require('path');
-
 module.exports = {
   webpack: (config, { isServer }) => {
     // Add an alias for the public directory
